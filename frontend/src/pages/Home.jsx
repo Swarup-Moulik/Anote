1d8ff67e71b49f5b84a0145f796f40f6b75aaeaa
Swarup Moulik <swarupmoulik1@gmail.com>
2025-11-06 19:43:12 +0530

    Initialize our project

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center text-2xl color-red-500'>
+      Hello World
+    </div>
+  )
+}
+
+export default Home
